08668177b88929b2630e763f84bccd113eece7e4
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-12 16:45:59 +0200

    Creating ViewModel and adding dependencies

--- app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksActivity.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksActivity.kt
@@ -2,6 +2,7 @@ package com.clakestudio.pc.readingassistant.books
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.clakestudio.pc.readingassistant.R
 
 class BooksActivity : AppCompatActivity() {
 
