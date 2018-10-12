ad1bf3bc0efcc20505cceaf286322bafe5f783ec
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-12 15:56:53 +0200

    MVVM databidning in kotlin second chance

---
+++ app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.clakestudio.pc.readingassistant.books
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
