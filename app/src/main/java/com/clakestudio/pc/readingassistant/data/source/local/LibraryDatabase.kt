9523f1a7d87ff078dc7a0bd943a9e96f59cde1ea
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-25 23:58:28 +0200

    Library database implementation

--- app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/LibraryDatabase.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/LibraryDatabase.kt
@@ -1,7 +1,9 @@
 package com.clakestudio.pc.readingassistant.data.source.local
 
 import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.content.Context
 import com.clakestudio.pc.readingassistant.data.Book
 
 @Database(entities = arrayOf(Book::class), version = 1)
@@ -11,6 +13,20 @@ abstract class LibraryDatabase: RoomDatabase() {
 
     companion object {
 
+        private var INSTANCE : LibraryDatabase? = null
+
+        private val lock = Any()
+
+        fun getInstance(context: Context): LibraryDatabase {
+
+            synchronized(lock) {
+                if (INSTANCE == null)
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, LibraryDatabase::class.java, "Books.db")
+                            .build()
+            }
+
+            return INSTANCE!!
+        }
     }
 
 }
