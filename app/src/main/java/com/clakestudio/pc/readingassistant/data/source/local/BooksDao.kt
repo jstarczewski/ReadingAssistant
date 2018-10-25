99279e51f2bb7af10d48ca0bb0d1c921b5313882
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-25 23:56:03 +0200

    Dao basic implementation

--- app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/BooksDao.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/BooksDao.kt
@@ -1,2 +1,25 @@
 package com.clakestudio.pc.readingassistant.data.source.local
 
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import com.clakestudio.pc.readingassistant.data.Book
+import io.reactivex.Flowable
+
+@Dao interface BooksDao {
+
+
+    @Query("SELECT * FROM Books")
+    fun getBooks() : Flowable<Book>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBook(book: Book)
+
+    /**
+     * More methods gonna be implemented when needed
+     * */
+
+
+
+}
--- app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/LibraryDatabase.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/data/source/local/LibraryDatabase.kt
@@ -1,2 +1,16 @@
 package com.clakestudio.pc.readingassistant.data.source.local
 
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import com.clakestudio.pc.readingassistant.data.Book
+
+@Database(entities = arrayOf(Book::class), version = 1)
+abstract class LibraryDatabase: RoomDatabase() {
+
+    abstract fun booksDao(): BooksDao
+
+    companion object {
+
+    }
+
+}
