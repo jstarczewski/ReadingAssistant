dbe9452aee46e7322d3d9dcc070d208af359955b
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-29 18:52:03 +0100

    RecyclerView implementation

--- app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksAdapter.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksAdapter.kt
@@ -6,33 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import com.clakestudio.pc.readingassistant.R
 import com.clakestudio.pc.readingassistant.data.Book
-import kotlinx.android.synthetic.main.book.view.*
 
 class BooksAdapter(
         private var books: List<Book>,
         private val booksViewModel: BooksViewModel
-) : RecyclerView.Adapter<BooksAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<BooksViewHolder>() {
+
+    /**
+     * Currently booksViewModel is not needed right now but, later when integration with
+     * certain CardViews will be implemented, we will be able to easily communicate with booksViewModel
+     * and for example open another activity
+     * */
 
     class ViewHolder(val bookItemView: View) : RecyclerView.ViewHolder(bookItemView)
 
-
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BooksAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BooksViewHolder {
         val view = LayoutInflater.from(parent!!.context).inflate(R.layout.book, parent, false) as View
-        return ViewHolder(view)
+        return BooksViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: BooksViewHolder?, position: Int) {
         /**
          * holder!! ??
          * */
-        holder!!.bookItemView.tvTitle.text = books[position].title
-        holder.bookItemView.tvAuthor.text = books[position].author
-        holder.bookItemView.tvNote.text = books[position].note
+
+        holder!!.bind(books[position])
+
     }
 
     override fun getItemCount(): Int {
         return books.size
     }
 
+    fun setBooksList(books: ArrayList<Book>) {
+        this.books = books
+        notifyDataSetChanged()
+    }
 
 }
