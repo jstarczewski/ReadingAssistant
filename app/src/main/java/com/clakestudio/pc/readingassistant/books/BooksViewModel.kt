d396be622eed604dbef98e62e96be2d565e2c3cf
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-30 20:36:01 +0100

    Wokring refreshing system

--- app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksViewModel.kt
+++ app/src/main/java/com/clakestudio/pc/readingassistant/books/BooksViewModel.kt
@@ -6,10 +6,12 @@ import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.OnLifecycleEvent
 import android.content.Context
 import android.databinding.ObservableArrayList
-import android.databinding.ObservableField
+import android.util.Log
 import com.clakestudio.pc.readingassistant.data.Book
 import com.clakestudio.pc.readingassistant.data.source.BooksRepository
 import com.clakestudio.pc.readingassistant.util.DisposableManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class BooksViewModel(
         context: Application,
@@ -18,24 +20,36 @@ class BooksViewModel(
 
     private val context: Context = context.applicationContext
 
-    val infoLabel = ObservableField<String>()
     val books: ObservableArrayList<Book> = ObservableArrayList()
 
-    fun start() {
-        booksRepository.saveBook(Book("ealo", "dziala", "czy nie dziala", "112"))
-        loadBooks()
-    }
 
     private fun loadBooks() {
 
+        /**
+         * Turns out that Disposable Manager is redutant and caused problems with refreshing ui
+         * -it will be remvoed after merge
+         *
+         * */
 
-        var bookss: List<Book> = booksRepository.getBooks()
-        for (book: Book in bookss)
-            this.books.add(book)
+       val disposable = booksRepository.getBooks()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    this.books.clear()
+                    this.books.addAll(it)
+                    loadBooks()
+                    Log.e("Lista ->", books.toString())
+                }, { t: Throwable -> t.printStackTrace() })
+        DisposableManager.getInstance().addDisposable(disposable)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun unsubscribeViewModel() {
+        DisposableManager.getInstance().clearDisposables()
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun start() {
+        loadBooks()
+    }
 }
