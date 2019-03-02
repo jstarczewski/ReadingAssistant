6a88f0e23bc0691a0ba155ab22842a04e6f5bbf6
jstarczewski <jan.a.starczewski@gmail.com>
2019-03-02 16:01:54 +0100

    Working simple espresso test

--- app/src/androidTest/java/com/clakestudio/pc/readingassistant/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/clakestudio/pc/readingassistant/ExampleInstrumentedTest.kt
@@ -1,12 +1,25 @@
 package com.clakestudio.pc.readingassistant
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.clakestudio.pc.readingassistant.addeditbook.AddEditBookActivity
+import com.clakestudio.pc.readingassistant.books.BooksActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +33,17 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.clakestudio.pc.readingassistant", appContext.packageName)
+    }
+
+    @Rule
+    @JvmField
+    var booksActivityTestRule = ActivityTestRule(BooksActivity::class.java)
+
+    @Test
+    fun testFabInteraction() {
+        Intents.init()
+        onView(withId(R.id.fab_addBook)).perform(click())
+        intended(hasComponent(AddEditBookActivity::class.java.name))
+        Intents.release()
     }
 }
